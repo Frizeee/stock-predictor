39a85421f4258fdf28a1d3728632d22f82910228
Máté Kádár <mate.kadar1@gmail.com>
2021-11-22 18:25:16 +0100

    Create a React app and publish it to GitHub Pages

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,25 +5,17 @@ import Main from './main';
 import Footer from './footer';
 import Devs from './devs';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   useRoutes,
 } from 'react-router-dom';
 
-const Routing = () => {
-  let routes = useRoutes([
-    { path: '/', element: <Main /> },
-    { path: '/devs', element: <Devs /> },
-  ]);
-  return routes;
-};
-
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <Header />
           <Routes>
             <Route exact path="/" element={<Main />} />
