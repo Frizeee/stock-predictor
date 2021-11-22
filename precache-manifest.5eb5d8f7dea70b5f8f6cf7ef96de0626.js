40fcaa731f835afbb0569826fb8629144c7084a1
Máté Kádár <mate.kadar1@gmail.com>
2021-11-22 18:14:31 +0100

    Updates

---
+++ precache-manifest.5eb5d8f7dea70b5f8f6cf7ef96de0626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5aafc9e1ef03d92c546ab70eccca37a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7f483a73a45c288a7a",
+    "url": "/static/css/2.51ff890b.chunk.css"
+  },
+  {
+    "revision": "ab37e3599d00c19bca17",
+    "url": "/static/css/main.b57f4624.chunk.css"
+  },
+  {
+    "revision": "1f7f483a73a45c288a7a",
+    "url": "/static/js/2.079cd936.chunk.js"
+  },
+  {
+    "revision": "eafb477bb8903e8249ea0b191ee5eb92",
+    "url": "/static/js/2.079cd936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab37e3599d00c19bca17",
+    "url": "/static/js/main.cf201dc8.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
